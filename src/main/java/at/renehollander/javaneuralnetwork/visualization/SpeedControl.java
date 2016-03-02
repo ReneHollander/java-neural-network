dad1763fe9d32436c25e908cff19a93cf942cbc0
Paul Kalauner <pkalauner@student.tgm.ac.at>
2016-03-02 20:46:07 +0100

    speed is now increased/decreased by 25ms per key press

--- src/main/java/at/renehollander/javaneuralnetwork/visualization/SpeedControl.java
+++ src/main/java/at/renehollander/javaneuralnetwork/visualization/SpeedControl.java
@@ -8,7 +8,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class SpeedControl implements GUIComponent, KeyListener {
-
+    private static final int STEP_SIZE = 25;
     private JSlider slider;
     private boolean paused = false;
 
@@ -43,9 +43,9 @@ public class SpeedControl implements GUIComponent, KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_PLUS || e.getKeyCode() == KeyEvent.VK_ADD) {
-            slider.setValue(slider.getValue() + 100);
+            slider.setValue(slider.getValue() + STEP_SIZE);
         } else if (e.getKeyCode() == KeyEvent.VK_MINUS || e.getKeyCode() == KeyEvent.VK_SUBTRACT) {
-            slider.setValue(slider.getValue() - 100);
+            slider.setValue(slider.getValue() - STEP_SIZE);
         } else if (e.getKeyCode() == KeyEvent.VK_P) {
             this.paused ^= true;
         }
